be3e379947e72fdc5316807087a383c79ce21fc3
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2026-06-30 11:51:55 +0500

    test: add negative test cases via dataProvider to testDeleteAccount; add testDeleteAccountUnsupportedMethod

--- src/test/java/io/github/matveyvolodin/api/client/AccountApiClient.java
+++ src/test/java/io/github/matveyvolodin/api/client/AccountApiClient.java
@@ -17,22 +17,22 @@ public class AccountApiClient {
     public AccountResponse createAccount(User user) {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
-                .formParam("name",          user.getName())
-                .formParam("email",         user.getEmail())
-                .formParam("password",      user.getPassword())
-                .formParam("title",         user.getTitle())
-                .formParam("birth_date",    user.getDayOfBirth())
-                .formParam("birth_month",   user.getMonthOfBirth())
-                .formParam("birth_year",    user.getYearOfBirth())
-                .formParam("firstname",     user.getFirstName())
-                .formParam("lastname",      user.getLastName())
-                .formParam("company",       user.getCompany())
-                .formParam("address1",      user.getAddress())
-                .formParam("address2",      user.getAddress2())
-                .formParam("country",       user.getCountry())
-                .formParam("zipcode",       user.getZipCode())
-                .formParam("state",         user.getState())
-                .formParam("city",          user.getCity())
+                .formParam("name", user.getName())
+                .formParam("email", user.getEmail())
+                .formParam("password", user.getPassword())
+                .formParam("title", user.getTitle())
+                .formParam("birth_date", user.getDayOfBirth())
+                .formParam("birth_month", user.getMonthOfBirth())
+                .formParam("birth_year", user.getYearOfBirth())
+                .formParam("firstname", user.getFirstName())
+                .formParam("lastname", user.getLastName())
+                .formParam("company", user.getCompany())
+                .formParam("address1", user.getAddress())
+                .formParam("address2", user.getAddress2())
+                .formParam("country", user.getCountry())
+                .formParam("zipcode", user.getZipCode())
+                .formParam("state", user.getState())
+                .formParam("city", user.getCity())
                 .formParam("mobile_number", user.getMobileNumber())
                 .when()
                 .post(CREATE_ACCOUNT_ENDPOINT)
@@ -50,22 +50,22 @@ public class AccountApiClient {
     public AccountResponse createAccountWithoutEmail(User user) {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
-                .formParam("name",          user.getName())
+                .formParam("name", user.getName())
                 // email formParam is intentionally omitted to test missing field validation
-                .formParam("password",      user.getPassword())
-                .formParam("title",         user.getTitle())
-                .formParam("birth_date",    user.getDayOfBirth())
-                .formParam("birth_month",   user.getMonthOfBirth())
-                .formParam("birth_year",    user.getYearOfBirth())
-                .formParam("firstname",     user.getFirstName())
-                .formParam("lastname",      user.getLastName())
-                .formParam("company",       user.getCompany())
-                .formParam("address1",      user.getAddress())
-                .formParam("address2",      user.getAddress2())
-                .formParam("country",       user.getCountry())
-                .formParam("zipcode",       user.getZipCode())
-                .formParam("state",         user.getState())
-                .formParam("city",          user.getCity())
+                .formParam("password", user.getPassword())
+                .formParam("title", user.getTitle())
+                .formParam("birth_date", user.getDayOfBirth())
+                .formParam("birth_month", user.getMonthOfBirth())
+                .formParam("birth_year", user.getYearOfBirth())
+                .formParam("firstname", user.getFirstName())
+                .formParam("lastname", user.getLastName())
+                .formParam("company", user.getCompany())
+                .formParam("address1", user.getAddress())
+                .formParam("address2", user.getAddress2())
+                .formParam("country", user.getCountry())
+                .formParam("zipcode", user.getZipCode())
+                .formParam("state", user.getState())
+                .formParam("city", user.getCity())
                 .formParam("mobile_number", user.getMobileNumber())
                 .when()
                 .post(CREATE_ACCOUNT_ENDPOINT)
@@ -83,22 +83,22 @@ public class AccountApiClient {
     public AccountResponse createAccountWithoutPassword(User user) {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
-                .formParam("name",          user.getName())
-                .formParam("email",         user.getEmail())
+                .formParam("name", user.getName())
+                .formParam("email", user.getEmail())
                 // password formParam is intentionally omitted to test missing field validation
-                .formParam("title",         user.getTitle())
-                .formParam("birth_date",    user.getDayOfBirth())
-                .formParam("birth_month",   user.getMonthOfBirth())
-                .formParam("birth_year",    user.getYearOfBirth())
-                .formParam("firstname",     user.getFirstName())
-                .formParam("lastname",      user.getLastName())
-                .formParam("company",       user.getCompany())
-                .formParam("address1",      user.getAddress())
-                .formParam("address2",      user.getAddress2())
-                .formParam("country",       user.getCountry())
-                .formParam("zipcode",       user.getZipCode())
-                .formParam("state",         user.getState())
-                .formParam("city",          user.getCity())
+                .formParam("title", user.getTitle())
+                .formParam("birth_date", user.getDayOfBirth())
+                .formParam("birth_month", user.getMonthOfBirth())
+                .formParam("birth_year", user.getYearOfBirth())
+                .formParam("firstname", user.getFirstName())
+                .formParam("lastname", user.getLastName())
+                .formParam("company", user.getCompany())
+                .formParam("address1", user.getAddress())
+                .formParam("address2", user.getAddress2())
+                .formParam("country", user.getCountry())
+                .formParam("zipcode", user.getZipCode())
+                .formParam("state", user.getState())
+                .formParam("city", user.getCity())
                 .formParam("mobile_number", user.getMobileNumber())
                 .when()
                 .post(CREATE_ACCOUNT_ENDPOINT)
@@ -117,21 +117,21 @@ public class AccountApiClient {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
                 // name formParam is intentionally omitted to test missing field validation
-                .formParam("email",         user.getEmail())
-                .formParam("password",      user.getPassword())
-                .formParam("title",         user.getTitle())
-                .formParam("birth_date",    user.getDayOfBirth())
-                .formParam("birth_month",   user.getMonthOfBirth())
-                .formParam("birth_year",    user.getYearOfBirth())
-                .formParam("firstname",     user.getFirstName())
-                .formParam("lastname",      user.getLastName())
-                .formParam("company",       user.getCompany())
-                .formParam("address1",      user.getAddress())
-                .formParam("address2",      user.getAddress2())
-                .formParam("country",       user.getCountry())
-                .formParam("zipcode",       user.getZipCode())
-                .formParam("state",         user.getState())
-                .formParam("city",          user.getCity())
+                .formParam("email", user.getEmail())
+                .formParam("password", user.getPassword())
+                .formParam("title", user.getTitle())
+                .formParam("birth_date", user.getDayOfBirth())
+                .formParam("birth_month", user.getMonthOfBirth())
+                .formParam("birth_year", user.getYearOfBirth())
+                .formParam("firstname", user.getFirstName())
+                .formParam("lastname", user.getLastName())
+                .formParam("company", user.getCompany())
+                .formParam("address1", user.getAddress())
+                .formParam("address2", user.getAddress2())
+                .formParam("country", user.getCountry())
+                .formParam("zipcode", user.getZipCode())
+                .formParam("state", user.getState())
+                .formParam("city", user.getCity())
                 .formParam("mobile_number", user.getMobileNumber())
                 .when()
                 .post(CREATE_ACCOUNT_ENDPOINT)
@@ -149,7 +149,7 @@ public class AccountApiClient {
     public AccountResponse createAccountWithPut(User user) {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
-                .formParam("email",    user.getEmail())
+                .formParam("email", user.getEmail())
                 .formParam("password", user.getPassword())
                 .when()
                 .put(CREATE_ACCOUNT_ENDPOINT)
@@ -167,7 +167,7 @@ public class AccountApiClient {
     public AccountResponse deleteAccount(User user) {
         Response response = given()
                 .spec(ApiConfig.getBaseSpec())
-                .formParam("email",    user.getEmail())
+                .formParam("email", user.getEmail())
                 .formParam("password", user.getPassword())
                 .when()
                 .delete(DELETE_ACCOUNT_ENDPOINT)
@@ -178,6 +178,79 @@ public class AccountApiClient {
         AccountResponse result = new AccountResponse();
         result.setResponseCode(response.jsonPath().getInt("responseCode"));
         result.setMessage(response.jsonPath().getString("message"));
+        return result;
+    }
+
+    @Step("Deleting account without email field")
+    public AccountResponse deleteAccountWithoutEmail(User user) {
+        Response response = given()
+                .spec(ApiConfig.getBaseSpec())
+                // email formParam is intentionally omitted to test missing field validation
+                .formParam("password", user.getPassword())
+                .when()
+                .delete(DELETE_ACCOUNT_ENDPOINT)
+                .then()
+                .extract()
+                .response();
+
+        AccountResponse result = new AccountResponse();
+        result.setResponseCode(response.jsonPath().getInt("responseCode"));
+        result.setMessage(response.jsonPath().getString("message"));
+        return result;
+    }
+
+    @Step("Deleting account without password field for user: {user.email}")
+    public AccountResponse deleteAccountWithoutPassword(User user) {
+        Response response = given()
+                .spec(ApiConfig.getBaseSpec())
+                .formParam("email", user.getEmail())
+                // password formParam is intentionally omitted to test missing field validation
+                .when()
+                .delete(DELETE_ACCOUNT_ENDPOINT)
+                .then()
+                .extract()
+                .response();
+
+        AccountResponse result = new AccountResponse();
+        result.setResponseCode(response.jsonPath().getInt("responseCode"));
+        result.setMessage(response.jsonPath().getString("message"));
+        return result;
+    }
+
+    @Step("Deleting account with wrong password for user: {user.email}")
+    public AccountResponse deleteAccountWrongPassword(User user) {
+        Response response = given()
+                .spec(ApiConfig.getBaseSpec())
+                .formParam("email", user.getEmail())
+                // Intentionally using a wrong password to test validation
+                .formParam("password", "wrongPassword123!")
+                .when()
+                .delete(DELETE_ACCOUNT_ENDPOINT)
+                .then()
+                .extract()
+                .response();
+
+        AccountResponse result = new AccountResponse();
+        result.setResponseCode(response.jsonPath().getInt("responseCode"));
+        result.setMessage(response.jsonPath().getString("message"));
+        return result;
+    }
+
+    @Step("Attempting to delete account with unsupported HTTP method PUT")
+    public AccountResponse deleteAccountWithPost(User user) {
+        Response response = given()
+                .spec(ApiConfig.getBaseSpec())
+                .formParam("email", user.getEmail())
+                .formParam("password", user.getPassword())
+                .when()
+                .post(DELETE_ACCOUNT_ENDPOINT)
+                .then()
+                .extract()
+                .response();
+
+        AccountResponse result = new AccountResponse();
+        result.setResponseCode(response.statusCode());
+        result.setMessage(response.jsonPath().getString("detail"));
         return result;
     }
 }
--- src/test/java/io/github/matveyvolodin/api/tests/AccountApiTest.java
+++ src/test/java/io/github/matveyvolodin/api/tests/AccountApiTest.java
@@ -7,23 +7,31 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AccountApiTest extends BaseApiTest{
 
+    @BeforeClass
+    public void setUp() {
+        super.setUp();
+        accountApiClient.createAccount(AccountApiTestData.DELETE_TEST_USER);
+    }
+
     @AfterClass
     public void tearDown() {
-        accountApiClient.deleteAccount(AccountApiTestData.TEST_USER);
+        safeDeleteAccount(AccountApiTestData.TEST_USER);
+        safeDeleteAccount(AccountApiTestData.DELETE_TEST_USER);
     }
 
     @Test(dataProvider = "registerUserData", dataProviderClass = AccountApiTestData.class)
     @Description("Verify POST /createAccount endpoint responses for different input scenarios")
     public void testCreateAccount(User user, String method, int expectedCode, String expectedMessage) {
         AccountResponse response = switch (method) {
-            case "createAccountWithoutEmail"    -> accountApiClient.createAccountWithoutEmail(user);
+            case "createAccountWithoutEmail" -> accountApiClient.createAccountWithoutEmail(user);
             case "createAccountWithoutPassword" -> accountApiClient.createAccountWithoutPassword(user);
-            case "createAccountWithoutName"     -> accountApiClient.createAccountWithoutName(user);
-            default                             -> accountApiClient.createAccount(user);
+            case "createAccountWithoutName" -> accountApiClient.createAccountWithoutName(user);
+            default -> accountApiClient.createAccount(user);
         };
 
         Allure.addAttachment("Test User", user.getEmail() + " / " + user.getPassword());
@@ -49,18 +57,34 @@ public class AccountApiTest extends BaseApiTest{
         );
     }
 
-    @Test
-    @Description("Verify that an existing user can be deleted successfully via the API")
-    public void testDeleteAccount() {
-        AccountResponse response = accountApiClient.deleteAccount(AccountApiTestData.TEST_USER);
+    @Test(dataProvider = "deleteUserData", dataProviderClass = AccountApiTestData.class)
+    @Description("Verify DELETE /deleteAccount endpoint responses for different input scenarios")
+    public void testDeleteAccount(User user, String method, int expectedCode, String expectedMessage) {
+        AccountResponse response = switch (method) {
+            case "deleteAccountWithoutEmail" -> accountApiClient.deleteAccountWithoutEmail(user);
+            case "deleteAccountWithoutPassword" -> accountApiClient.deleteAccountWithoutPassword(user);
+            case "deleteAccountWrongPassword" -> accountApiClient.deleteAccountWrongPassword(user);
+            default -> accountApiClient.deleteAccount(user);
+        };
 
-        //Service returns 200 if deleted successfully, but it should be 204.
-        Allure.step("Verify the response code '200'", () ->
-        Assert.assertEquals(response.getResponseCode(),200)
+        Allure.step("Verify the response code '" + expectedCode + "'", () ->
+        Assert.assertEquals(response.getResponseCode(),expectedCode)
         );
-        Allure.step("Verify the response message 'Account deleted!'", () ->
-        Assert.assertEquals(response.getMessage(), "Account deleted!")
+        Allure.step("Verify the response message '" + expectedMessage + "'", () ->
+        Assert.assertEquals(response.getMessage(), expectedMessage)
         );
     }
 
+    @Test
+    @Description("Verify that POST method is not allowed for DELETE /deleteAccount endpoint")
+    public void testDeleteAccountUnsupportedMethod() {
+        AccountResponse response = accountApiClient.deleteAccountWithPost(AccountApiTestData.DELETE_TEST_USER);
+
+        Allure.step("Verify the response code '405'", () ->
+                Assert.assertEquals(response.getResponseCode(), 405)
+        );
+        Allure.step("Verify the response message", () ->
+                Assert.assertEquals(response.getMessage(), "Method \"POST\" not allowed.")
+        );
+    }
 }
